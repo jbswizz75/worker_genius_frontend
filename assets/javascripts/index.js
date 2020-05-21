648eabd96771c63eb916bb7c2bd0f095640866ca
daproclaima <sn.itpro@gmail.com>
2020-05-21 10:27:06 +0200

    adds visual model html and css export of Nicolas

--- assets/javascripts/index.js
+++
@@ -1,8 +0,0 @@
-let $ = require('jquery')
-global.$ = global.jquery = $
-require('bootstrap')
-require('./datepicker.common.js')
-require('./datepicker.min.js')
-require('./fontawesome.js')
-require('@chenfengyuan/datepicker/i18n/datepicker.fr-FR')
-
